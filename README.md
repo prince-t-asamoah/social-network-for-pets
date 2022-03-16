beb80345db315ca1e15e1488d7a3682f5f97bc72
PRINCE TWUMASI ASAMOAH <prince.t.asamoah@gmail.com>
2022-03-16 07:38:00 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# PetBook
+
+A simple social network for pets. You can view a pet’s profile (which contains their name, bio, and friend list)
+and navigate to other profiles. There is also a profile directory where you can see all of the profiles.
+
+## Screenshot
+
+### Homepage
+
+![PetBook (Homepage)](./resources/images/homepage.jpeg)
+
+### Profile Page
+
+![PetBook (Profile Page)](./resources/images/profile-page.jpeg)
+
+## Technology Used
+
+- HTML
+- CSS
+- JavaScript (ReactJs)
+
+## Source
+
+&copy; [Codecademy.com](https://codecademy.com) 2022.
---
+++ resources/scripts/Profile.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { fetchUserData, cancelFetch } from "./dataFetcher";
+import { Userlist } from "./Userlist";
+
+export class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { userData: null };
+  }
+  loadUserData() {
+    this.setState({ userData: null });
+    this.fetchID = fetchUserData(this.props.username, (userData) => {
+      this.setState({ userData });
+    });
+  }
+  componentDidMount() {
+    this.loadUserData();
+  }
+  componentDidUpdate(prevProps) {
+    if (this.props.username !== prevProps.username) {
+      cancelFetch(this.fetchID);
+      this.loadUserData();
+    }
+  }
+  componentWillUnmount() {
+    cancelFetch(this.fetchID);
+  }
+  render() {
+    const isLoading = this.state.userData === null ? true : false;
+    let name, bio, friends;
+    let className = "Profile";
+
+    if (isLoading) {
+      className += " loading";
+      name = "Loading...";
+      bio = "Bio goes here";
+      friends = [];
+    } else {
+      name = this.state.userData.name;
+      bio = this.state.userData.bio;
+      friends = this.state.userData.friends;
+    }
+    return (
+      <div className={className}>
+        <div className="profile-picture">
+          {!isLoading && (
+            <img src={this.state.userData.profilePictureUrl} alt="" />
+          )}
+        </div>
+        <div className="profile-body">
+          <h2>{name}</h2>
+          <h3>@{this.props.username}</h3>
+          <p>{bio}</p>
+          <h3>My friends</h3>
+          <Userlist usernames={friends} onChoose={this.props.onChoose} />
+        </div>
+      </div>
+    );
+  }
+}
